6aa8ae8f63561559c6a46a718a0f73c7378a6e22
KenLiau <itl.kglab@gmail.com>
2025-11-13 14:54:28 +0800

    實作DB Table部分欄位加密功能

--- app/src/main/java/com/itl/kglab/noteEncryptorManager/repository/MainRepository.kt
+++ app/src/main/java/com/itl/kglab/noteEncryptorManager/repository/MainRepository.kt
@@ -5,11 +5,13 @@ import com.itl.kglab.noteEncryptorManager.data.db.NoteInfoColumn
 import com.itl.kglab.noteEncryptorManager.data.db.updateInfo.InfoSettingUpdate
 import com.itl.kglab.noteEncryptorManager.data.db.updateInfo.NoteInfoUpdate
 import com.itl.kglab.noteEncryptorManager.data.pref.PreferencesManager
+import com.itl.kglab.noteEncryptorManager.tools.CryptographicUtility
 import com.itl.kglab.noteEncryptorManager.tools.SettingInfo
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.io.encoding.Base64
 
 class MainRepository @Inject constructor (
     private val preferencesManager: PreferencesManager,
@@ -32,19 +34,19 @@ class MainRepository @Inject constructor (
     override suspend fun createNoteInfo(info: NoteInfoColumn) =
         withContext(ioDispatcher) {
             val dao = database.noteInfoDao()
-            dao.insertInfo(info)
+            dao.insertInfo(encryptInfoColumn(info))
         }
 
     override suspend fun getNoteList(): List<NoteInfoColumn> =
         withContext(ioDispatcher) {
             val dao = database.noteInfoDao()
-            dao.getInfoList()
+            dao.getInfoList().map { decryptInfoColumn(it) }
         }
 
     override suspend fun getNoteInfoById(id: Long): NoteInfoColumn =
         withContext(ioDispatcher) {
             val dao = database.noteInfoDao()
-            dao.getInfoById(id)
+            decryptInfoColumn(dao.getInfoById(id))
         }
 
     override suspend fun deleteNoteInfo(info: NoteInfoColumn) =
@@ -63,14 +65,53 @@ class MainRepository @Inject constructor (
     override suspend fun updateNoteInfo(info: NoteInfoUpdate) {
         withContext(ioDispatcher) {
             val dao = database.noteInfoDao()
-            dao.updateNoteInfo(info)
+            val encryptedInfo = info.copy(
+                note = encryptMessage(info.note)
+            )
+            dao.updateNoteInfo(encryptedInfo)
         }
     }
 
     override suspend fun updateNoteSampleSetting(info: InfoSettingUpdate) {
         withContext(ioDispatcher) {
             val dao = database.noteInfoDao()
-            dao.updateInfoSetting(info)
+            val encryptedInfo = info.copy(
+                inputText = encryptMessage(info.inputText),
+                outputText = encryptMessage(info.outputText),
+                sampledMessage = encryptMessage(info.sampledMessage)
+            )
+            dao.updateInfoSetting(encryptedInfo)
         }
+    }
+
+    /**
+     *  加解密Func
+     */
+    private fun encryptInfoColumn(info: NoteInfoColumn): NoteInfoColumn {
+        return info.copy(
+            note = encryptMessage(info.note),
+            inputText = encryptMessage(info.inputText),
+            outputText = encryptMessage(info.outputText),
+            sampledMessage = encryptMessage(info.sampledMessage)
+        )
+    }
+
+    private fun decryptInfoColumn(encryptedInfo: NoteInfoColumn): NoteInfoColumn {
+        return encryptedInfo.copy(
+            note = decryptMessage(encryptedInfo.note),
+            inputText = decryptMessage(encryptedInfo.inputText),
+            outputText = decryptMessage(encryptedInfo.outputText),
+            sampledMessage = decryptMessage(encryptedInfo.sampledMessage)
+        )
+    }
+
+    private fun encryptMessage(message: String): String {
+        val encryptArray = CryptographicUtility.encrypt(message.toByteArray())
+        return Base64.encode(encryptArray)
+    }
+
+    private fun decryptMessage(message: String): String {
+        val encryptedArray = Base64.decode(message)
+        return String(CryptographicUtility.decrypt(encryptedArray))
     }
 }
